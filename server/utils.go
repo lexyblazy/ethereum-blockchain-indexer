2b5bb07b20d07b8e8b2f1c428d10375557eafca8
lexyblazy <adekola.olalekan01@gmail.com>
2023-02-03 12:13:22 +0100

    only allow application/json content-type

--- server/utils.go
+++ server/utils.go
@@ -1,10 +1,17 @@
 package server
 
 import (
+	"net/http"
 	"strings"
 )
 
 func GetParamFromRequestURL(urlPath string) string {
 	i := strings.LastIndexByte(urlPath, '/')
 	return (urlPath[i+1:])
+}
+
+func IsJsonContentType(r *http.Request) bool {
+	headerContentType := r.Header.Get("Content-Type")
+
+	return headerContentType == "application/json"
 }
