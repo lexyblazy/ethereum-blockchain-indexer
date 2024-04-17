0ec4f5c5eac6b9ac8eb63f04634774f2c6ee7b53
lexyblazy <adekola.olalekan01@gmail.com>
2024-04-17 02:57:12 +0100

    add bloom filter config and setup column family handles

--- db/rocksdb.go
+++ db/rocksdb.go
@@ -7,51 +7,72 @@ import (
 	gorocksdb "github.com/linxGnu/grocksdb"
 )
 
+const (
+	cfDefault = iota
+	cfHeight
+	cfAddress
+	cfBlockTxs
+	cfTransactions
+)
+
+var cfNames = []string{"default", "height", "addresses", "blockTxs", "transactions"}
+
 type RocksDB struct {
-	path string
-	db   *gorocksdb.DB
-	wo   *gorocksdb.WriteOptions
-	ro   *gorocksdb.ReadOptions
+	path  string
+	db    *gorocksdb.DB
+	wo    *gorocksdb.WriteOptions
+	ro    *gorocksdb.ReadOptions
+	cfh   []*gorocksdb.ColumnFamilyHandle
+	cache *gorocksdb.Cache
 }
 
-func createDBOptions(dbCacheSize int) *gorocksdb.Options {
+func createDBOptions(cache *gorocksdb.Cache, maxOpenFiles int) *gorocksdb.Options {
 	bbto := gorocksdb.NewDefaultBlockBasedTableOptions()
-	bbto.SetBlockCache(gorocksdb.NewLRUCache(uint64(dbCacheSize)))
+	bbto.SetBlockCache(cache)
 	bbto.SetBlockSize(32 << 10) // 32kB
+	bbto.SetFilterPolicy(gorocksdb.NewBloomFilter(float64(10)))
+
 
 	opts := gorocksdb.NewDefaultOptions()
 	opts.SetBlockBasedTableFactory(bbto)
 	opts.SetCreateIfMissing(true)
+	opts.SetCreateIfMissingColumnFamilies(true)
+	opts.SetMaxOpenFiles(maxOpenFiles)
 
 	return opts
 }
 
-func openDb(path string, dbCacheSize int) (*gorocksdb.DB, error) {
-	opts := createDBOptions(dbCacheSize)
+func openDb(path string, cache *gorocksdb.Cache) (*gorocksdb.DB, []*gorocksdb.ColumnFamilyHandle, error) {
+	opts := createDBOptions(cache, 1<<14)
 
-	db, err := gorocksdb.OpenDb(opts, path)
+	cfOptions := []*gorocksdb.Options{opts, opts, opts, opts, opts}
+
+	db, cfh, err := gorocksdb.OpenDbColumnFamilies(opts, path, cfNames, cfOptions)
 
 	if err != nil {
 		log.Fatal("Failed to establish database connection")
-		return nil, err
+		return nil, nil, err
 	}
 
-	return db, nil
+	return db, cfh, nil
 }
 
 func NewConn(path string, dbCacheSize int) (*RocksDB, error) {
 
-	db, err := openDb(path, dbCacheSize)
+	cache := gorocksdb.NewLRUCache(uint64(dbCacheSize))
+	db, cfh, err := openDb(path, cache)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return &RocksDB{
-		path: path,
-		db:   db,
-		wo:   gorocksdb.NewDefaultWriteOptions(),
-		ro:   gorocksdb.NewDefaultReadOptions(),
+		path:  path,
+		db:    db,
+		wo:    gorocksdb.NewDefaultWriteOptions(),
+		ro:    gorocksdb.NewDefaultReadOptions(),
+		cfh:   cfh,
+		cache: cache,
 	}, nil
 
 }
