6de385e596e4f62057109f5610d64deb032a6e22
lexyblazy <adekola.olalekan01@gmail.com>
2024-04-20 00:41:57 +0100

    WIP worker sync

--- worker/worker.go
+++ worker/worker.go
@@ -5,26 +5,47 @@ import (
 	"indexer/bchain"
 	"indexer/db"
 	"log"
+	// "sync"
+	// "time"
+	// "log"
 )
 
 type Worker struct {
-	bc *bchain.BlockChain
-	db *db.RocksDB
+	bc           *bchain.BlockChain
+	db           *db.RocksDB
+	workersCount int
 }
 
-func NewWorker(bc *bchain.BlockChain, index *db.RocksDB, nodeUrl string) *Worker {
+func NewWorker(bc *bchain.BlockChain, index *db.RocksDB, workersCount int) *Worker {
 	return &Worker{
-		bc: bc,
-		db: index,
+		bc:           bc,
+		db:           index,
+		workersCount: workersCount,
 	}
 }
 
-func (w *Worker) Sync(fromheight, toHeight int) {
 
-	latestBlock, err := w.bc.GetBestBlockHeight()
-	if err != nil {
-		log.Fatal("Failed to get latest block")
+func (w *Worker) Sync(fromHeight, toHeight int) {
+
+	h, _, _ := w.db.GetBestBlock()
+
+	if fromHeight > toHeight {
+		toHeight = fromHeight
 	}
 
-	fmt.Println("latestBlock", latestBlock, "fromHeight", fromheight, "toHeight", toHeight)
+	if h == 0 {
+		fmt.Println("No block in the database, this is a fresh sync")
+
+		// sync block from startHeight to the remote best block
+
+		rpcBestBlock, err := w.bc.GetBestBlockHeight()
+
+		if err != nil {
+			log.Fatal("Failed to get rpcBestBlock for Fresh sync")
+		}
+
+		log.Println("Remote RPC block best height is", rpcBestBlock)
+
+	}
+
 }
